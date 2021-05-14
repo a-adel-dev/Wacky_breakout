3ab560edb3bbad487c5a47a12a528883bfc4a8b3
a-adel-dev <58854098+a-adel-dev@users.noreply.github.com>
2021-05-14 10:23:00 +0200

    edit

--- Assets/Scripts/Util/BallSpawner.cs
+++ Assets/Scripts/Util/BallSpawner.cs
@@ -6,6 +6,10 @@ public class BallSpawner : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefab;
     Timer timer;
+
+    bool retrySpawn = false;
+    Vector2 spawnLocationMin;
+    Vector2 spawnLocationMax;
     // Start is called before the first frame update
 
 
@@ -13,6 +17,18 @@ public class BallSpawner : MonoBehaviour
     {
         timer = GetComponent<Timer>();
         RunSpawnTimer();
+
+        GameObject tempBall = Instantiate<GameObject>(ballPrefab);
+        BoxCollider2D collider = tempBall.GetComponent<BoxCollider2D>();
+        float ballColliderHalfWidth = collider.size.x / 2;
+        float ballColliderHalfHeight = collider.size.y / 2;
+        spawnLocationMin = new Vector2(
+            tempBall.transform.position.x - ballColliderHalfWidth,
+            tempBall.transform.position.y - ballColliderHalfHeight);
+        spawnLocationMax = new Vector2(
+            tempBall.transform.position.x + ballColliderHalfWidth,
+            tempBall.transform.position.y + ballColliderHalfHeight);
+        Destroy(tempBall);
     }
 
     private void Update()
@@ -22,10 +38,24 @@ public class BallSpawner : MonoBehaviour
             SpawnNewBall();
             RunSpawnTimer();
         }
+
+        if (retrySpawn)
+        {
+            SpawnNewBall();
+        }
+
     }
     public void SpawnNewBall()
     {
-        Instantiate(ballPrefab, new Vector2(0f,0f), Quaternion.identity);
+        if (Physics2D.OverlapArea(spawnLocationMin, spawnLocationMax) == null)
+        {
+            retrySpawn = false;
+            Instantiate(ballPrefab);
+        }
+        else
+        {
+            retrySpawn = true;
+        }
     }
 
     void RunSpawnTimer ()
